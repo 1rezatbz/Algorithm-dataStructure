9248f4e65daae4c3ced61009402c050e5631ae79
sgrfazli <1rezartabriz@gmail.com>
2023-03-05 14:35:58 +0000

    add Counting sort and Shell Sort

---
+++ src/sortAlgo/CountingSorter.java
@@ -0,0 +1,27 @@
+package sortAlgo;
+import java.util.Arrays;
+public class CountingSorter {
+    public void sort(Integer []data){
+        int [] temp = new int[11];
+        for(int i =0; i< data.length;i++){
+            temp[data[i]] = temp[data[i]] +1;
+        }
+        int current =0;
+        for (int i=0;i<temp.length;i++ ){
+            for (int j=0; j<temp[i];j++){
+                data[current] =i;
+                current++;
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args){
+        CountingSorter sorter = new CountingSorter();
+        Integer[] data = new Integer [] {3, 10, 2, 4, 2, 6, 9, 5, 7, 2, 8, 10, 3,9};
+        sorter.sort(data);
+        System.out.println(Arrays.deepToString(data));
+
+    }
+}
--- src/sortAlgo/shellSorter.java
+++ src/sortAlgo/shellSorter.java
@@ -1,14 +1,44 @@
 package sortAlgo;
 
 import java.util.Arrays;
-import java.util.Random;
 
 public class shellSorter {
-    public void sort(){}
-    public void insertionSortWith(int[] data, int startIndex, int gap){
+
+    public static void main(String[] args){
+//        Integer[] data={13,5,8,3,2,1,4,7,10,11,9,6,12,0};
+//        new shellSorter().sort(data);
+//        System.out.println(Arrays.deepToString(data));
+        System.out.println(maxKnuthSeqNumber(1000));
 
     }
-    private int maxKnuthSeqNumber(int size){
+
+    public void sort(Integer[] data) {
+        if (data == null || data.length == 0) {return;}
+        int knuthNum = maxKnuthSeqNumber(data.length);
+        while (knuthNum >= 1) {
+            for (int i = 0; i < knuthNum; i++) {
+                // insertion is used to sort  each pass
+                insertionSortWithGap(data, i, knuthNum);
+            }
+            knuthNum = (knuthNum - 1) / 3; //decrease the gap
+        }
+    }
+
+    public void insertionSortWithGap(Integer[] data, int startIndex, int gap) {
+        int i = startIndex;
+        while (i < data.length) {
+            int current = data[i];
+            int j = i - gap;
+            while (j >= 0 && data[j] >= current) {
+                data[j + gap] = data[j];
+                j = j - gap;
+            }
+            data[j + gap] = current;
+            i = i + gap;
+        }
+    }
+
+    public static    int maxKnuthSeqNumber(int size){
         int h =1;
         while (h < size / 3) { h = 3 * h + 1; }
         return h;}
