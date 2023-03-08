b2dd8a042729925ce8d4a0f67dc13c51890b0c67
sgrfazli <1rezartabriz@gmail.com>
2023-03-08 11:24:05 +0000

    complete Hash map Data structure

--- src/HashTable/hashTable.java
+++ src/HashTable/hashTable.java
@@ -3,8 +3,10 @@ package HashTable;
 //implementation of a Hash Table Data Structure.
 
 public class hashTable {
+    // table size
     private static final int TABLE_SIZE = 100;
     private Record[] tableData = new Record[TABLE_SIZE];
+    // Put method: takes a key-value pair, calculates the hash code, and stores the Record in the corresponding slot
     public void put(Object key, Object value){
         int keyCode = key.hashCode();
         int step = 0;
@@ -14,7 +16,7 @@ public class hashTable {
         }
         tableData[slot]= new Record(key,value);
     }
-
+    // KeyExists method: checks if a given key exists in the hash table
     public boolean keyExists( Object key){
         int keyCode = key.hashCode();
         int step =0;
@@ -25,6 +27,8 @@ public class hashTable {
         if (tableData[slot]!= null){return true;}
         return false;
     }
+    // Get method: retrieves the value corresponding to the given key from the hash table
+
     private Object get(Object key){
         int keyCode = key.hashCode();
         int step= 0;
@@ -36,15 +40,18 @@ public class hashTable {
         return null;
 
     }
+    // Hash method: calculates the hash code of a given key, based on the table size and the step
     private int hash(int key, int step){
         if(step ==0){return key % TABLE_SIZE;}
         return (key % TABLE_SIZE + step*step)% TABLE_SIZE;
     }
+
+    // isEmpty method: checks if a given slot in the hash table is empty
     private boolean isEmpty(int slot) {return tableData[slot]==null;}
 
 
 
-
+// Record class: defines a Record, which consists of a key and a data value
     private class Record{
         Object key;
         Object data;
@@ -64,12 +71,10 @@ public class hashTable {
         ht.put("8", 80);
         ht.put("9", 90);
         ht.put("5", 50);
-        System.out.println(ht.keyExists("2"));
-        System.out.println(ht.keyExists("4"));
-        System.out.println(ht.get(2));
-        System.out.println(ht.get("4"));
-
-
+        System.out.println(ht.keyExists("2")); // false
+        System.out.println(ht.keyExists("4")); // true
+        System.out.println(ht.get(2)); // null
+        System.out.println(ht.get("4")); // 40
     }
 
 }
